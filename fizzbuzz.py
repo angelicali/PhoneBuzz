7414e2c2722a3659547ccfac889331ba050acb9d
Angelica <liyunshuangelica@gmail.com>
2016-11-09 13:39:38 -0800

    basic setup

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import twilio.twiml
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET','POST'])
+def hello():
+	resp = twilio.twiml.Response()
+	resp.say("Hello Monkey!" , voice="alice")
+
+	return str(resp)
+
+if __name__ == "__main__":
+	app.run(debug=True)
+
