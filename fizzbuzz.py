f52514714cffd73b574f01453e96a033945fb719
Angelica <liyunshuangelica@gmail.com>
2016-11-09 13:46:23 -0800

    add gather

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import twilio.twiml
 
 app = Flask(__name__)
@@ -6,7 +6,17 @@ app = Flask(__name__)
 @app.route("/", methods=['GET','POST'])
 def hello():
 	resp = twilio.twiml.Response()
-	resp.say("Hello Monkey!" , voice="alice")
+	
+	if 'Digits' in request.values:
+		num = request.values['Digits']
+		
+		resp.say('You entered '+str(num))
+
+
+	with resp.gather(numDigits=1) as gather:
+		gather.say('Enter a number')
+
+	resp.redirect('/')
 
 	return str(resp)
 
