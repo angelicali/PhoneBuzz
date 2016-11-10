6fb67926dcc668dfd6a530caebd2bfd726bd297d
Angelica <liyunshuangelica@gmail.com>
2016-11-09 17:04:25 -0800

    add dialing

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,15 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import twilio.twiml
 from twilio.util import RequestValidator
+from twilio.rest.lookups import TwilioLookupsClient
+from twilio.rest import TwilioRestClient
 
 from hashlib import sha1
 import hmac 
 
 
-
+account_sid = "ACbba02f681a40e42defdc0ae3c7cd0acd"
 auth_token = "2e0586523d70a82d17d94c2f23c45d51"
 validator = RequestValidator(auth_token)
 url = "https://phonebuzz-yl.herokuapp.com/"
+
+
 
 
 app = Flask(__name__)
@@ -41,10 +45,38 @@ def fizzbuzz(n):
 def allfizzbuzz(n):
 	return ' '.join([fizzbuzz(i) for i in range(1,n+1)])
 
+def valid_phone_number(num_str):
+	client = TwilioLookupsClient(account=account_sid, token=auth_token)
+	try:
+		number = client.phone_numbers.get(num_str)
+		number = number.phone_number  
+		return number
+	except:
+		return None
 
 
 @app.route("/", methods=['GET','POST'])
-def hello():
+def dialPhoneBuzz():
+	if request.method == 'GET':
+		return render_template("dial.html")
+	else:
+		if 'tel' in request.form:
+			num = request.form['tel']
+			num = valid_phone_number(num)
+			if num:
+				client = TwilioRestClient(account=account_sid, token=auth_token)
+				# Make the call
+				call = client.calls.create(to= num,  # Any phone number
+								           from_="+12179797039", # Must be a valid Twilio number
+								           url="https://phonebuzz-yl.herokuapp.com/fizzbuzz")
+				return "Calling %s"%num
+		return render_template("dial.html", error_msg = "invalid phone number")
+
+
+
+
+@app.route("/fizzbuzz", methods=['GET','POST'])
+def phoneBuzz():
 	resp = twilio.twiml.Response()
 
 	if request.method == 'POST':
@@ -61,9 +93,12 @@ def hello():
 	with resp.gather(timeout=4) as gather:
 		gather.say('Enter a number to play the phone buzz game. ')
 
-	resp.redirect('/')
+	resp.redirect('/fizzbuzz')
 
 	return str(resp)
+
+
+
 
 
 
